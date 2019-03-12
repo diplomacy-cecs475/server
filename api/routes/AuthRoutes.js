5ed62cd0b882ca8876e76ad6b610e2bd77fb650c
Morgan SIMON <morgan.simon@epitech.eu>
2019-03-12 15:35:30 -0700

    Remove useless routes

--- api/routes/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const AuthController = require('../controllers/AuthController');
-
-const router = express.Router();
-
-router.get('/test', AuthController.test);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const app = express();
 const server = http.Server(app);
 const io = socket(server);
-const router = express.Router();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
@@ -15,8 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-router.use('/auth', require('./api/routes/AuthRoutes'));
-
-app.use('/api', router);
+app.get('/', (req, res) => {
+  res.status(200).send('Server root');
+});
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
