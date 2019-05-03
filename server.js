645dc6080785748a4c48d8aa9b6921ae8564edb3
server <server@main-server.us-west2-a.c.space-citizen.internal>
2019-05-03 02:21:21 +0000

    use build folder with express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('@google-cloud/debug-agent').start();
+//require('@google-cloud/debug-agent').start();
 const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
@@ -8,15 +8,15 @@ const path = require('path');
 const app = express();
 const server = http.Server(app);
 const io = socket(server);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4042;
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/build/static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
